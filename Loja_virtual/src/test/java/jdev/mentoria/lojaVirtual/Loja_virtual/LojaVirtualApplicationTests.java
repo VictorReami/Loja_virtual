3c6c71a382a1437c75c1adc89ee7c3c6e4edf53b
VictorReami <victor.mreami@gmail.com>
2024-02-11 10:14:41 -0300

    Primeiro Commit

---
+++ Loja_virtual/src/test/java/jdev/mentoria/lojaVirtual/Loja_virtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojaVirtual.Loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

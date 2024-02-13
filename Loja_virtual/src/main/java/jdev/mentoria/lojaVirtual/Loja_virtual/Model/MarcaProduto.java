21ae2fe6fec9762db8bdf5c1c60589caa9272518
VictorReami <victor.mreami@gmail.com>
2024-02-13 09:37:23 -0300

    2.1 - Criando o projeto Spring Boot Rest API com Maven

---
+++ Loja_virtual/src/main/java/jdev/mentoria/lojaVirtual/Loja_virtual/Model/MarcaProduto.java
@@ -0,0 +1,27 @@
+package jdev.mentoria.lojaVirtual.Loja_virtual.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+
+    private String nomeDesc;
+
+}

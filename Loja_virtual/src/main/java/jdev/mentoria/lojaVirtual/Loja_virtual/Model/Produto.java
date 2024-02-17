00a6d603c239d04308275ff1d9f09074b35e8ad8
VictorReami <victor.mreami@gmail.com>
2024-02-17 09:24:52 -0300

    2.6 - Criando Banco e Classes com Spring Data JPA - Parte 5

---
+++ Loja_virtual/src/main/java/jdev/mentoria/lojaVirtual/Loja_virtual/Model/Produto.java
@@ -0,0 +1,71 @@
+package jdev.mentoria.lojaVirtual.Loja_virtual.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "produto")
+@SequenceGenerator(name = "seq_produto", sequenceName = "seq_produto", allocationSize = 1, initialValue = 1)
+public class Produto implements Serializable {
+
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
+    private Long id;
+
+    private String tipoUnidade;
+
+    private String nome;
+
+    private Boolean ativo = Boolean.TRUE;
+
+    @Column(columnDefinition = "text", length = 2000)
+    private String descricao;
+
+    /* Nota item notaproduto - ASSOCIAR */
+
+    private Double peso; /* EX: 1000.55*/
+
+    private Double largura;
+
+    private Double altura;
+
+    private Double profundidade;
+
+    private BigDecimal valorVenda = BigDecimal.ZERO;
+
+    private Integer qtdeEstoque = 0;
+
+    private Integer qtdeAlertaEstoque = 0;
+
+    private String linkYoutube;
+
+    private Boolean alertaQtdeEstoque = Boolean.FALSE;
+
+    private Integer qtdeClick = 0;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(getId(), produto.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}

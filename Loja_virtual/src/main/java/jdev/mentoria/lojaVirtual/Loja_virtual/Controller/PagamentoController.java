71e2029054ab18d22549244a602df2cbbffbd6ee
VictorReami <victor.mreami@gmail.com>
2024-12-15 08:23:17 -0300

    11.2 - Criando Tela e Controller de Pagamento

---
+++ Loja_virtual/src/main/java/jdev/mentoria/lojaVirtual/Loja_virtual/Controller/PagamentoController.java
@@ -0,0 +1,23 @@
+package jdev.mentoria.lojaVirtual.Loja_virtual.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.Serializable;
+
+@Controller
+public class PagamentoController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @GetMapping(value = "/pagamento/idVendaCompra")
+    public ModelAndView pagamento(@PathVariable (value = "idVendaCompra", required = false) String idVendaCompra){
+
+        return new ModelAndView("Pagamento");
+
+    }
+}
